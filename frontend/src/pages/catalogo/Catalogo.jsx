1efed16b06e6a45df50a90b260b5811b375f464f
muraisnchz <mauriciojaviersnchz@gmail.com>
2026-04-16 21:55:42 -0300

    modificaciones para implementación del .env en fronted y uso correcto de useEffect

--- frontend/src/pages/catalogo/Catalogo.jsx
+++ frontend/src/pages/catalogo/Catalogo.jsx
@@ -7,24 +7,34 @@ import './Catalogo.css';
 const Catalogo = () => {
   const [productos, setProductos] = useState([]);
   const [cargando, setCargando] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const cargarCatalogo = async () => {
-      const data = await obtenerProductos();
-      setProductos(data);
-      setCargando(false);
+      try {
+        const data = await obtenerProductos();
+        setProductos(data);
+
+      } catch (err) {
+        setError("Error al cargar el catálogo. Por favor, inténtalo de nuevo más tarde.");
+        console.error("Detalle del error:", err);
+      } finally {
+        setCargando(false);
+      }
     };
     cargarCatalogo();
   }, []);
 
   if (cargando) return <h2>Cargando el catálogo de joyas...</h2>;
 
+  if (error) return <h2 style={{ color: 'red' }}>{error}</h2>;
+
   return (
     <div className="catalogo-container">
       <h1>Catálogo de Joyas</h1>
       <div className="catalogo-grid">
         {productos.map((producto) => (
-          <ProductCard 
+          <ProductCard
             key={producto._id}
             _id={producto._id}
             nombre={producto.nombre}
