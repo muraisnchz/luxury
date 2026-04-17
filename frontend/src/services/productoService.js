3d35422f3f5110806f57dce5710127310c31f84e
muraisnchz <mauriciojaviersnchz@gmail.com>
2026-04-16 21:55:42 -0300

    modificaciones para implementación del .env en fronted y uso correcto de useEffect

--- frontend/src/services/productoService.js
+++ frontend/src/services/productoService.js
@@ -1,22 +1,12 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:3000/api/productos';
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/productos`;
 
 export const obtenerProductos = async () => {
-  try {
-    const respuesta = await axios.get(API_URL);
-        return await respuesta.data;
-  } catch (error) {
-    console.error("Error al obtener los productos:", error);
-    return [];
-  }
+  const respuesta = await axios.get(API_URL);
+  return respuesta.data;
 };
 
 export const obtenerProductoPorId = async (id) => {
-  try {
-    const respuesta = await axios.get(`${API_URL}/${id}`);
-    return await respuesta.data;
-  } catch (error) {
-    console.error("Error al obtener el producto:", error);
-    return null;
-  }
+  const respuesta = await axios.get(`${API_URL}/${id}`);
+  return respuesta.data;
 };
