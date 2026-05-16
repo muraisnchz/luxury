4ca5c3591c2e04ff5fe9976f661d7fcf1a748962
muraisnchz <mauriciojaviersnchz@gmail.com>
2026-05-16 15:13:46 -0300

    agregado el lápiz en el catalogo cuando se loguea un admin

--- frontend/src/components/productCard/ProductCard.jsx
+++ frontend/src/components/productCard/ProductCard.jsx
@@ -1,33 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './ProductCard.css';
-import Button from '../button/Button';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaEdit } from "react-icons/fa";
+import "./ProductCard.css";
+import Button from "../button/Button";
 
-const ProductCard = ({ _id, nombre, imagenUrl, descripcion, precio, categoriaId, onAddToCart }) => {
+const ProductCard = ({
+  _id,
+  nombre,
+  imagenUrl,
+  descripcion,
+  precio,
+  categoriaId,
+  onAddToCart,
+  esAdmin,
+}) => {
   return (
     <div className="product-card">
       {/* Ponemos una imagen por defecto por si alguna joya no tiene URL cargada en la BD */}
-      <img 
-        src={imagenUrl || 'https://img.freepik.com/vector-premium/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093.jpg'} 
-        alt={`Imagen de ${nombre}`} 
-        className="product-image" 
+      {esAdmin && (
+        <Link
+          to={`/admin/editarProducto/${_id}`}
+          className="btn-editar-flotante"
+          title="Editar Producto"
+        >
+          <FaEdit />
+        </Link>
+      )}
+
+      <img
+        src={
+          imagenUrl ||
+          "https://img.freepik.com/vector-premium/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093.jpg"
+        }
+        alt={`Imagen de ${nombre}`}
+        className="product-image"
       />
       <div className="product-info">
         <h3 className="product-title">{nombre}</h3>
-        
-        
+
         <span className="product-category">{categoriaId?.nombre}</span>
-        
+
         <p className="product-description">{descripcion}</p>
         <span className="product-price">${precio}</span>
-        
+
         <div className="product-actions">
           {/* Botón para ir a la vista de Detalle */}
           <Link to={`/producto/${_id}`}>
-            <Button texto="Ver detalle" color="lila" size="grande" efecto="redondeado" />
+            <Button
+              texto="Ver detalle"
+              color="lila"
+              size="grande"
+              efecto="redondeado"
+            />
           </Link>
-          
-          
         </div>
       </div>
     </div>
